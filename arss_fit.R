a320455f7eb1a92ab75656e6009b28eeb08e9bac
Mark Scheuerell <scheuerl@uw.edu>
2019-08-01 14:58:53 -0700

    minor changes to fitting params

--- arss_fit.R
+++ arss_fit.R
@@ -1,12 +1,6 @@
 
-if(!require("rstan")) {
-  install.packages("rstan")
-  library("rstan")
-}
-if(!require("here")) {
-  install.packages("here")
-  library("here")
-}
+library("rstan")
+library("here")
 
 ## set stan options
 rstan_options(auto_write = TRUE)
@@ -17,8 +11,8 @@ stan_dir <- here("exec")
 
 ## stan options
 stan_model <- "arss_nc.stan"
-stan_ctrl <- list(max_treedepth = 25, adapt_delta = 0.9999)
-stan_mcmc <- list(iter = 4000, warmup = 2000, chains = 4, thin = 10, refresh = 0)
+stan_ctrl <- list(max_treedepth = 15, adapt_delta = 0.999)
+stan_mcmc <- list(iter = 6000, warmup = 5000, chains = 4, thin = 10, refresh = 0)
 
 ## initial values
 init_vals <- function(chain_id = 1, n_time) {
@@ -30,40 +24,35 @@ init_vals <- function(chain_id = 1, n_time) {
   )
 }
 
-
-
-
-nn <- 200
-
+## AR(1) params for simulation
+nn <- 50
 phi <- 0.7
-
-SD_proc <- 0.4
-
-SD_obs <- 0.1
-
+SD_proc <- sqrt(0.4)
+SD_obs <- sqrt(0.1)
 init_state <- 0
 
+## init states & proc errors
 xx <- ww <- rnorm(nn, 0, SD_proc)
 
+## states
 xx[1] <- phi * init_state + ww[1]
 for(t in 2:nn) {
   xx[t] <- phi * xx[t-1] + ww[t]
 }
 
+## obs data
 yy <- xx + rnorm(nn, 0, SD_obs)
-
-
 
 
 ## data list for Stan
 dat <- list(n_year = nn, yy = yy)
 
-
 ## initial values
 init_ll <- lapply(1:stan_mcmc$chains,
                   function(id) init_vals(chain_id = id, n_time = nn)
-)
+                  )
 
+## fit the model
 fit <- stan(file = file.path(stan_dir, stan_model),
             data = dat,
             pars = c("phi", "SD_proc", "SD_obs"),
@@ -75,7 +64,12 @@ fit <- stan(file = file.path(stan_dir, stan_model),
             thin = stan_mcmc$thin,
             refresh = stan_mcmc$refresh)
 
+
+## SHIELD YOUR EYES BEFORE EXECUTING THESE COMMANDS
+
+## examine fits
 print(fit, pars = c("SD_proc", "SD_obs", "phi"))
 
 traceplot(fit, pars = c("SD_proc", "SD_obs", "phi"), inc_warmup = FALSE, nrow = 3)
 
+shinystan::launch_shinystan(fit)
