dc2c5931f5032e14ec0c92a4fb077789005c3908
eric-ward <eric.ward@noaa.gov>
2019-10-04 05:16:57 -0700

    increase iterations

--- R/big_board_sim_fit.R
+++ R/big_board_sim_fit.R
@@ -42,7 +42,7 @@ n_toss <- 20
 ## stan options
 stan_model <- "marss_diag_unequal_Q_diag_equal_prior_R.stan"
 stan_ctrl <- list(max_treedepth = 25, adapt_delta = 0.999)
-stan_mcmc <- list(iter = 2000, warmup = 1000, chains = 3, thin = 1, refresh = 0)
+stan_mcmc <- list(iter = 3000, warmup = 2000, chains = 3, thin = 1, refresh = 0)
 
 ## interaction matrices (B)
 ## index 1 is bottom of food web; 4 is top
