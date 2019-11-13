60f21cc601287204c2bd27b6ad3c984fdb0c16c0
eric-ward <eric.ward@noaa.gov>
2019-11-12 17:01:06 -0800

    replicates from 20 -> 40

--- R/make_grid.R
+++ R/make_grid.R
@@ -1,7 +1,7 @@
 
 # Proposed simulation framework / datasets
 set.seed(123)
-replicates = 20 # number of replicates/combination of different parameters
+replicates = 40 # number of replicates/combination of different parameters
 
 grid = expand.grid("obs_sd" = c(0.2, 0.4, 0.8),
   "pro_sd" = c(0.1, 0.2, 0.4),
@@ -20,9 +20,9 @@ grid$keep[which(grid$b_CV==1)] = 1
 # 2. for interatction effects, high CV variances (obs_CV, pro_CV==1) and 
 grid$keep[which(grid$obs_CV==1 & grid$pro_CV==1)] = 1
 
-grid$seed = .Random.seed[grid$iter]
-grid = grid[which(grid$keep==1),]
 
+grid = grid[which(grid$keep==1),]
+grid$seed = seq(1,nrow(grid))
 saveRDS(grid, file="grid.rds")
 
 # EW: I propose creating the above grid, but *not necessarily running all of those models*
