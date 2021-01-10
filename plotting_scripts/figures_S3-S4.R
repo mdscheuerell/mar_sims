a7d5f3d06cde7c2a350d2244481f8db3bb7a2150
Eric Ward <5046884+ericward-noaa@users.noreply.github.com>
2021-01-10 06:12:11 -0800

    Add files via upload

--- plotting_scripts/figures_S3-S4.R
+++ plotting_scripts/figures_S3-S4.R
@@ -12,12 +12,12 @@ grid$sd_pro_est = sqrt(grid$Q)
   
 # make basic plots of
 pdf("plots/Figure_S3.pdf")
-g1 = ggplot(dplyr::filter(grid, b_CV==1),
+g1 = ggplot(dplyr::filter(grid, b_CV==1,sd_obs_est < 1),
        aes(x = as.factor(""), y=sd_obs_est)) +
   geom_boxplot(col="darkblue",fill="darkblue",alpha=0.4,outlier.shape = NA) +
   ylab(expression(paste("Estimated observation error ",sigma))) +
   geom_hline(aes(yintercept = obs_sd),col="red",alpha=0.3) +
-  facet_grid(obs_sd_label~ pro_sd_label, scale="free") + 
+  facet_grid(obs_sd_label~ pro_sd_label) + 
   xlab("Estimate")+
   theme_bw() + 
   theme(legend.position='none',strip.background = element_rect(color="black",fill="white"))
@@ -29,12 +29,12 @@ g1
 dev.off()
 
 pdf("plots/Figure_S4.pdf")
-g2 = ggplot(dplyr::filter(grid, b_CV==1),
+g2 = ggplot(dplyr::filter(grid, b_CV==1, sd_pro_est<1),
   aes(x = as.factor(""), y=sd_pro_est)) +
   geom_boxplot(col="darkblue",fill="darkblue",alpha=0.4,outlier.shape = NA) +
   ylab(expression(paste("Estimated process error ",sigma))) +
   geom_hline(aes(yintercept = pro_sd),col="red",alpha=0.3) +
-  facet_grid(obs_sd_label~ pro_sd_label, scale="free")+
+  facet_grid(obs_sd_label~ pro_sd_label)+
   xlab("Estimate")+
   theme_bw() + 
   theme(legend.position='none',strip.background = element_rect(color="black",fill="white"))
